6636613b04a59072029c950d35bc586ad6caf460
Mumtazrullah <anwarullahmumtaz404@gmail.com>
2023-10-09 19:31:37 +0700

    Tambah file index dan script serta ubah isi readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # belajar-javascript
-Belajar
+Sedikit materi tentang javascript
+
+Pemula programmer sering salah tentang JavaScript dan Java. Ada yang mengira bahwa Java dan JavaScript adalah bahasa pemrograman yang sama, padahal itu berbeda. Ternyata kata Java dimasukkan pada JavaScript hanyalah untuk strategi pemasaran agar mendapatkan penerimaan. Java adalah bahasa pemrograman lain, tidak ada hubungannya dengan JavaScript.
+
+Java adalah bahasa pemrograman yang lebih dulu populer dan memiliki reputasi yang sangat bagus pada waktu itu. Walaupun namanya ada kata “Java” nya, tapi dua bahasa pemrograman ini benar-benar berbeda, tidak ada hubungannya sama sekali.
---
+++ script.js
@@ -0,0 +1,71 @@
+// Menampilkan pesan dengan alert
+alert("Buka console untuk melihat isi website");
+
+// Output teks "Angka dengan angka"
+console.log("Angka dengan angka\n");
+
+// Variabel angka
+let x = 10;
+let y = 2;
+
+// Menampilkan nilai dari x dan y
+console.log("x =", x,"\n");
+console.log("y =", y, "\n\n\n");
+
+// Operasi penjumlahan
+console.log("Penjumlahan\n");
+console.log(x, "+", y, " = ", x + y, "\n");
+
+// Operasi pengurangan
+console.log("Pengurangan\n");
+console.log(x, "-", y, " = ", x - y, "\n");
+
+// Operasi perkalian
+console.log("Perkalian\n");
+console.log(x, "*", y, " = ", x * y, "\n");
+
+// Operasi eksponen (pangkat)
+console.log("Eksponen\n");
+console.log(x, "**", y, " = ", x ** y, "\n");
+
+// Operasi pembagian
+console.log("Pembagian\n");
+console.log(x, ":", y, " = ", x / y, "\n");
+
+// Operasi modulus
+console.log("Modulus\n");
+console.log(x, "%", y, " = ", x % y, "\n");
+
+// Operasi increment (penambahan 1)
+console.log("Increment\n");
+console.log("++x = ", ++x,"\n");
+
+// Operasi decrement (pengurangan 1)
+console.log("Decrement\n");
+console.log("--y = ", --y, "\n\n\n");
+
+// Output teks "Teks dengan teks"
+console.log("Teks dengan teks\n");
+
+// Variabel teks
+let a = "Selamat";
+let b = "Datang";
+
+// Menggabungkan teks
+let jumlah = a + " " + b;
+
+// Menampilkan hasil penjumlahan teks dengan teks
+console.log("\nPenjumlahan teks dengan teks");
+console.log(jumlah);
+
+// Output teks "Angka dengan teks"
+console.log("\nAngka dengan teks");
+
+// Variabel angka dan teks
+let angka = 10001;
+let teks = "Malam";
+
+// Menampilkan hasil penjumlahan angka dengan teks
+console.log("Penjumlahan angka dengan teks");
+let campur = angka + " " + teks;
+console.log(campur);
